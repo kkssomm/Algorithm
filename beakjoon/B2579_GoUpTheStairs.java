7d9126af1fc85b494af54a6dc81fa8ec5f32a7ce
kkssomm <asdfx1415@naver.com>
2020-03-31 06:08:00 +0900

    dp

---
+++ beakjoon/B2579_GoUpTheStairs.java
@@ -0,0 +1,31 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class B2579_GoUpTheStairs {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] stairs = new int[n + 1];
+		int[] dp = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			stairs[i] = sc.nextInt();
+		}
+
+		
+		dp[1] = stairs[1];
+		if (n != 1) {
+			dp[2] = stairs[1] + stairs[2];
+
+			for (int i = 3; i <= n; i++) {
+				dp[i] = Math.max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
+			}
+		}
+		System.out.println(dp[n]);
+
+		sc.close();
+	}
+
+}
